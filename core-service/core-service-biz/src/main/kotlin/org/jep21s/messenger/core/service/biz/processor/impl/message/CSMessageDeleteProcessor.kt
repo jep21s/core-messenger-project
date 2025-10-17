c79617ba3266e0f0c98048db49892ea0d8e353c4
jep21s <ilyaplesanov92@gmail.com>
2025-10-17 15:38:11 +0300

    biz-homework: refactor

--- core-service/core-service-biz/src/main/kotlin/org/jep21s/messenger/core/service/biz/processor/impl/message/CSMessageDeleteProcessor.kt
+++ core-service/core-service-biz/src/main/kotlin/org/jep21s/messenger/core/service/biz/processor/impl/message/CSMessageDeleteProcessor.kt
@@ -1,7 +1,6 @@
 package org.jep21s.messenger.core.service.biz.processor.impl.message
 
 import org.jep21s.messenger.core.service.biz.cor.runChain
-import org.jep21s.messenger.core.service.biz.extention.putModelResp
 import org.jep21s.messenger.core.service.biz.processor.CSProcessor
 import org.jep21s.messenger.core.service.biz.processor.impl.message.stubs.stubsMessageDeletion
 import org.jep21s.messenger.core.service.common.context.CSContext
--- core-service/core-service-biz/src/main/kotlin/org/jep21s/messenger/core/service/biz/processor/impl/message/status/CSMessageStatusUpdateProcessor.kt
+++ core-service/core-service-biz/src/main/kotlin/org/jep21s/messenger/core/service/biz/processor/impl/message/status/CSMessageStatusUpdateProcessor.kt
@@ -1,7 +1,6 @@
 package org.jep21s.messenger.core.service.biz.processor.impl.message.status
 
 import org.jep21s.messenger.core.service.biz.cor.runChain
-import org.jep21s.messenger.core.service.biz.extention.putModelResp
 import org.jep21s.messenger.core.service.biz.processor.CSProcessor
 import org.jep21s.messenger.core.service.biz.processor.impl.message.status.stubs.stubsMessageStatusUpdation
 import org.jep21s.messenger.core.service.common.context.CSContext
