4e6aa9a66105bee576cb3f4a307ceb0ec32c8fa5
jep21s <ilyaplesanov92@gmail.com>
2025-11-25 16:25:04 +0300

    repo-homework: refactor after review

--- core-service/core-service-app-web/src/main/kotlin/org/jep21s/messenger/core/service/app/web/scope/LazyScheduleScope.kt
+++ core-service/core-service-app-web/src/main/kotlin/org/jep21s/messenger/core/service/app/web/scope/LazyScheduleScope.kt
@@ -3,26 +3,27 @@ package org.jep21s.messenger.core.service.app.web.scope
 import io.ktor.server.application.Application
 import java.io.Serializable
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class LazyScheduleScope(
   initializer: Application.() -> CoroutineScope,
-  lock: Any? = null,
 ) : Serializable {
   private var initializer: (Application.() -> CoroutineScope)? = initializer
 
   @Volatile
   private var _value: Any? = UNINITIALIZED_VALUE
 
-  private val lock = lock ?: this
+  private val mutex = Mutex()
 
-  fun value(app: Application): CoroutineScope {
+  suspend fun value(app: Application): CoroutineScope {
     val _v1 = _value
     if (_v1 !== UNINITIALIZED_VALUE) {
       @Suppress("UNCHECKED_CAST")
       return _v1 as CoroutineScope
     }
 
-    return synchronized(lock) {
+    return mutex.withLock {
       val _v2 = _value
       if (_v2 !== UNINITIALIZED_VALUE) {
         @Suppress("UNCHECKED_CAST") (_v2 as CoroutineScope)
